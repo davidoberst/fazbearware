618eab0982cf863606045861b7a21d946ba0147e
davidoberst <juandavidbustosperez82@gmail.com>
2026-04-18 21:09:30 -0500

    encrypt and decrypt

--- main.py
+++ main.py
@@ -4,24 +4,52 @@ import os
 import sys
 from cryptography.fernet import Fernet 
 
-#generate and save key
-key = Fernet.generate_key()
-with open("key.key", "wb") as file_key:
-    file_key.write(key)
+key = Fernet.generate_key() #KEY TO ENCRYPT AND DECRYPT
+def encrypt():
+ #save key
+ with open("KEY", "wb") as file_key:
+     file_key.write(key)
+ #load key
+ loaded_key = Fernet(key)
 
-#load key
-loaded_key = Fernet(key)
-
-#Read original file
-with open("prueba.txt", "rb") as f:
+ #Read original file
+ with open("prueba.txt", "rb") as f:
     original_data = f.read()
+ #encrypt data, fernet is gonna to encrypt the content in the variable "original_data" using the loaded key
 
-#encrypt data, fernet is gonna to encrypt the content in the variable "original_data" using the loaded key
-encrypted_data = loaded_key.encrypt(original_data)
+ encrypted_data = loaded_key.encrypt(original_data)
 
-#save encrypted file
-with open("documento.txt.encrypted", "wb") as f_encrypted:
+ #save encrypted file
+ with open("prueba.txt", "wb") as f_encrypted:
     f_encrypted.write(encrypted_data)
+
+def decrypt():
+   input("decrypt?")
+   #read file key 
+   with open("KEY") as file_key:
+      key_to_decrypt = file_key.read()
+      
+   decryptor = Fernet(key_to_decrypt)
+
+   #read encrypted data
+   with open("prueba.txt","rb") as f:
+      encrypted_data = f.read()
+      original_data = decryptor.decrypt(encrypted_data)
+    #restore
+   with open("prueba.txt","wb") as restored_file:
+      restored_file.write(original_data)
+    
+encrypt()
+decrypt()  
+  
+
+
+
+
+
+
+
+
 
 def fazwin():
     pygame.init() #init pygame
