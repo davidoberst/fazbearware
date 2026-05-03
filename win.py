b1912147ff7593e1fe026664d2a8785571f035f7
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-03 17:58:04 -0500

    window ransom advise update

--- win.py
+++ win.py
@@ -7,6 +7,7 @@ import tkinter as tk
 window = tk.Tk()
 window.title("FOUR FILES ARE ENCRYPTED BY FAZBEARWARE")
 window.geometry("800x600")
+window.resizable(False, False)
 window.configure(bg="black")
 title = tk.Label(
     window,
@@ -16,12 +17,17 @@ title = tk.Label(
     font=("Georgia",22),
     padx=20,pady=20
 )
+title.pack(side="top", pady=(20, 0)) 
 
-title.pack(side="top", fill="x", anchor="n")
+instrucciones = tk.Label(
+    window,
+    text="Your important files are encrypted. Many of your documents, photos, videos, and other files are no longer accessible because they have been encrypted.. If you are busy looking for a way to recover your files, do not waste your time. The only way to restore your files is to purchase a unique decryptor key.",
+    fg="red",
+    bg="black",
+    font=("Georgia", 14),
+    justify="center",
+    wraplength=760
+)
 
-
-
-
-
-title.pack(expand=True)
+instrucciones.pack(side="top", pady=5)
 window.mainloop()
